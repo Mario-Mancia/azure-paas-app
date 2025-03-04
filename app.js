539f8df1c0da00d47965bd79dbef213afc0cf7b6
Mario-Mancía <mrmancia1999@gmail.com>
2025-03-04 00:40:10 -0600

    Segundo commit

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 import express from "express";
 import cors from "cors";
+import { sequelize } from "./config/dbconfig.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const PORT = 3000 || 3001;
-app.listen(PORT, ()=> {
-   try {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+import studentRoutes from './routes/students.js';
+app.use('/students', studentRoutes);
 
-   }
-   catch (error) {
-    console.error("No se pudo iniciar el servidor: ", error)
-   }
-});
+const PORT = 3000;
+
+sequelize.sync()
+   .then(()=> {
+      console.log('Base de Datos sincronizada');
+      app.listen(PORT, (res) => {
+         try {
+            console.log(`Servidor corriendo en http://localhost:${PORT}`);
+            
+         }
+         catch (error) {
+            console.error("No se pudo iniciar el servidor: ", error);
+         }
+      });
+   })
+      .catch(err=>{console.error('Error al establecer la conexión con la base de datos', err)});
